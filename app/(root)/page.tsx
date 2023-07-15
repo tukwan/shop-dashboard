093ba9c80a6de15279d7f5842cb182261af81200
tukwan <nick@atlhk.net>
2023-07-15 09:46:06 +0700

    init clerk

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,9 @@
-import { Button } from "@/components/ui/button"
+import { UserButton } from "@clerk/nextjs"
 
-export default function Home() {
+export default function SetupPage() {
   return (
     <div className="p-4">
-      <Button size="default" variant="outline">
-        Test
-      </Button>
+      <UserButton afterSignOutUrl="/" />
     </div>
   )
 }
