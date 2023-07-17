fd3afcdd6f941b12a48d18c282bdf17d4a09e786
tukwan <nick@atlhk.net>
2023-07-17 14:35:09 +0700

    store api

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -42,7 +42,7 @@ export const StoreModal = () => {
 
       const res = await axios.post("/api/stores", values)
 
-      toast.success("Store created")
+      window.location.assign(`/${res.data.id}`)
     } catch (error) {
       toast.error("Something went wrong.")
     } finally {
