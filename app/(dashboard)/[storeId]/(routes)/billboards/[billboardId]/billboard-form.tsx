44e0321107f8dfd11076cf4b1de426ce44a02b06
tukwan <nick@atlhk.net>
2023-07-21 18:52:58 +0700

    add billboard

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/billboard-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/billboard-form.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Store } from "@prisma/client"
+import { Billboard } from "@prisma/client"
 import { Trash } from "lucide-react"
 import * as z from "zod"
 import { useState } from "react"
@@ -26,17 +26,18 @@ import { AlertModal } from "@/components/modals/alert-modal"
 import { ApiAlert } from "@/components/ui/api-alert"
 import { useOrigin } from "@/hooks/use-origin"
 
-interface SettingsFormProps {
-  initialData: Store
-}
-
 const formSchema = z.object({
-  name: z.string().min(1),
+  label: z.string().min(1),
+  imageUrl: z.string().min(1),
 })
 
-type SettingsFormValues = z.infer<typeof formSchema>
+type BillboardFormValues = z.infer<typeof formSchema>
 
-export function SettingsForm({ initialData }: SettingsFormProps) {
+interface BillboardFormProps {
+  initialData: Billboard | null
+}
+
+export function BillboardForm({ initialData }: BillboardFormProps) {
   const params = useParams()
   const router = useRouter()
   const origin = useOrigin()
@@ -44,12 +45,20 @@ export function SettingsForm({ initialData }: SettingsFormProps) {
   const [open, setOpen] = useState(false)
   const [loading, setLoading] = useState(false)
 
-  const form = useForm<SettingsFormValues>({
+  const title = initialData ? "Edit billboard" : "Create billboard"
+  const description = initialData ? "Edit a billboard" : "Add a new billboard"
+  const toastMessage = initialData ? "Billboard updated." : "Billboard crated."
+  const action = initialData ? "Save changes" : "Create"
+
+  const form = useForm<BillboardFormValues>({
     resolver: zodResolver(formSchema),
-    defaultValues: initialData,
+    defaultValues: initialData || {
+      label: "",
+      imageUrl: "",
+    },
   })
 
-  const onSubmit = async (data: SettingsFormValues) => {
+  const onSubmit = async (data: BillboardFormValues) => {
     try {
       setLoading(true)
       await axios.patch(`/api/stores/${params.storeId}`, data)
@@ -86,20 +95,19 @@ export function SettingsForm({ initialData }: SettingsFormProps) {
         loading={loading}
       />
       <div className="flex items-center justify-between">
-        <Heading
-          title="Settings"
-          description="Manage store preferences"
-        />
-        <Button
-          disabled={loading}
-          variant="destructive"
-          size="icon"
-          onClick={() => {
-            setOpen(true)
-          }}
-        >
-          <Trash className="h-4 w-4" />
-        </Button>
+        <Heading title={title} description={description} />
+        {initialData && (
+          <Button
+            disabled={loading}
+            variant="destructive"
+            size="icon"
+            onClick={() => {
+              setOpen(true)
+            }}
+          >
+            <Trash className="h-4 w-4" />
+          </Button>
+        )}
       </div>
       <Separator />
       <Form {...form}>
@@ -110,14 +118,14 @@ export function SettingsForm({ initialData }: SettingsFormProps) {
           <div className="grid grid-cols-3 gap-8">
             <FormField
               control={form.control}
-              name="name"
+              name="label"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Name</FormLabel>
+                  <FormLabel>Label</FormLabel>
                   <FormControl>
                     <Input
                       disabled={loading}
-                      placeholder="Store name"
+                      placeholder="Billboard label"
                       {...field}
                     />
                   </FormControl>
@@ -127,16 +135,11 @@ export function SettingsForm({ initialData }: SettingsFormProps) {
             />
           </div>
           <Button disabled={loading} className="ml-auto" type="submit">
-            Save changes
+            {action}
           </Button>
         </form>
       </Form>
       <Separator />
-      <ApiAlert
-        title="NEXT_PUBLIC_API_URL"
-        description={`${origin}/api/${params.storeId}`}
-        variant="public"
-      />
     </>
   )
 }
